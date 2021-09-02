5bbd8d4fa286d2953a29697038a2299cce1446f9
guillote87 <guillermo27@gmail.com>
2021-09-01 21:08:52 -0300

    carga de productos al carrito

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,6 +4,7 @@ const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/productsData.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
+
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const userController = {
@@ -15,8 +16,27 @@ const userController = {
     },
    
     cart: (req, res) => {
-        res.render("productCart")
-    }
-}
+        const productsCartPath = path.join(__dirname, '../data/cartData.json');
+        const cart = JSON.parse(fs.readFileSync(productsCartPath, 'utf-8'));
+        console.log (cart)
+        res.render("productCart",{cart: cart})
+    },
+    store:(req,res) =>{
+        const productsCartPath = path.join(__dirname, '../data/cartData.json');
+        const cart = JSON.parse(fs.readFileSync(productsCartPath, 'utf-8'));
+  
+                  let id = req.params.id;
+                products.forEach(producto => {
+                    if (producto.id == id) {
+                       cart.push(producto)
+                    }
+                })
+                console.log(cart)
+                let cartJSON = JSON.stringify(cart)
+            fs.writeFileSync(productsCartPath, cartJSON)
+            res.redirect("/users/cart")
+        }}
 
+
+        
 module.exports = userController
--- src/routes/users.js
+++ src/routes/users.js
@@ -5,6 +5,10 @@ const userController = require ("../controllers/userController")
 
 router.get("/login", userController.login)
 router.get("/register", userController.register)
+
+
 router.get ("/cart", userController.cart)
+router.put("/cart/:id", userController.store)
+
 
 module.exports = router
